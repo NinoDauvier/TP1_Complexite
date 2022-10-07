42d5e113e90ab5368f395fd910154a93727b98b2
Nino <dauvier.nino@gmail.com>
2022-10-07 22:01:38 +0200

    Turing working with basic visual graph with both matrix and list

--- src/MainGraph.java
+++ src/MainGraph.java
@@ -5,14 +5,14 @@ import java.util.LinkedList;
 public class MainGraph {
 
     public static void main(String[] args) throws IOException {
-        GraphInterface graph = new GraphMatrix("C:\\Users\\DELL\\Desktop\\TD programation\\TP1_Complexité\\src\\GraphAdjacencyExample");
+        GraphInterface graph = new GraphMatrix("src\\GraphAdjacencyExample");
         System.out.println(GraphMethod.isVoidZone(graph, new LinkedList<>(Arrays.asList(0, 1, 4))));
         System.out.println(GraphMethod.isVoidZone(graph, new LinkedList<>(Arrays.asList(0, 1, 3))));
         System.out.println(GraphMethod.voidZoneMaximal(graph));
         System.out.println(GraphMethod.voidZoneMaximum(graph));
         System.out.println(GraphMethod.decentVoidZone(graph));
 
-        GraphInterface graphList = new GraphList("C:\\Users\\DELL\\Desktop\\TD programation\\TP1_Complexité\\src\\GraphAdjacencyExample");
+        GraphInterface graphList = new GraphList("src\\GraphAdjacencyExample");
         System.out.println(GraphMethod.isVoidZone(graph, new LinkedList<>(Arrays.asList(0, 1, 4))));
         System.out.println(GraphMethod.isVoidZone(graph, new LinkedList<>(Arrays.asList(0, 1, 3))));
         System.out.println(GraphMethod.voidZoneMaximal(graph));
