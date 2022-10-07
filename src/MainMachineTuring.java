9d6be6d363281fccf57212a2cdf6bb77e7a8a2e8
Nino <dauvier.nino@gmail.com>
2022-10-07 22:04:39 +0200

    Turing working with basic visual graph with both matrix and list

--- src/MainMachineTuring.java
+++ src/MainMachineTuring.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class MainMachineTuring {
     public static void main(String[] args) throws InterruptedException, IOException {
 
-        TuringMachine myMachine = new TuringMachine("C:\\Users\\DELL\\Desktop\\TD programation\\TP1_Complexité\\src\\Turing_prog");
+        TuringMachine myMachine = new TuringMachine("src\\Turing_prog");
         myMachine.calculAndPrint();
     }
 }
